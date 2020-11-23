c755e84c250214262bd20a01fc967dda4c945155
Jeongyun Lee <suejlemo@gmail.com>
2020-11-23 22:09:26 +0100

    backround noti update

---
+++ app/src/main/java/com/example/medictionary/NotificationActivity.kt
@@ -0,0 +1,15 @@
+package com.example.medictionary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.app.NotificationCompat
+
+class NotificationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notification2)
+
+
+
+    }
+}
